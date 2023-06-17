1b8ca45d9589d18b098985c3098e51925dd57299
Suryansh Anand <suryansh.anand42@gmail.com>
2023-06-18 01:53:36 +0530

    Fix: Styling for Add/Edit Product view

--- routes/admin.js
+++ routes/admin.js
@@ -12,9 +12,12 @@ const router = express.Router();
 
 router.get('/add-product', getAddProduct);
 router.post('/add-product', postAddProduct);
+
 router.get('/edit-product/:productId', getEditProduct);
 router.post('/edit-product', postEditProduct);
+
 router.post('/delete-product', postDeleteProduct);
+
 router.get('/products', getProducts);
 
 module.exports = router;
