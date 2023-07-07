3e0c75d43383f39c2e10726150d301802a8e0e05
Suryansh Anand <suryansh.anand42@gmail.com>
2023-07-07 23:53:28 +0530

    Feat: Responsive navbar.

---
+++ public/navbar.js
@@ -0,0 +1,17 @@
+/* global document */
+
+// Toggle the mobile menu
+const mobileMenuButton = document.querySelector('.sm\\:hidden button');
+const mobileMenu = document.querySelector('#mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
+
+// Toggle the user menu
+const userMenuButton = document.querySelector('#user-menu-button');
+const userMenu = document.querySelector('#user-menu');
+
+userMenuButton.addEventListener('click', () => {
+  userMenu.classList.toggle('hidden');
+});
